cfa2ada0a5d352f17f96313644f298664d530b6c
Sharath_MS <sharath.ms@tapplent.com>
2018-12-18 02:28:39 +0530

    added activity and fragment with layouts

--- app/src/main/java/com/andy/andymvp/DashBoardActivity.java
+++ app/src/main/java/com/andy/andymvp/DashBoardActivity.java
@@ -2,12 +2,26 @@ package com.andy.andymvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.andy.andymvp.fragment.RecyclerFragment;
 
 public class DashBoardActivity extends AppCompatActivity {
+
+    private final String FRAGMENT_TAG = "RECYCLER_FRAGMENT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        if (savedInstanceState == null) {
+            RecyclerFragment recyclerFragment = new RecyclerFragment();
+            recyclerFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().replace(android.R.id.content, recyclerFragment, FRAGMENT_TAG).commit();
+            Log.e("TAG", "savedInstanceState == null");
+        } else {
+            RecyclerFragment test = (RecyclerFragment) getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG);
+            Log.e("TAG", "savedInstanceState != null");
+        }
     }
 }
---
+++ app/src/main/java/com/andy/andymvp/fragment/RecyclerFragment.java
@@ -0,0 +1,30 @@
+package com.andy.andymvp.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andy.andymvp.R;
+
+public class RecyclerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recycler, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
